5a58d90d2f48978a32450cf0d4f9b078dc2ceae4
김성은 <kse012050@naver.com>
2024-01-03 15:05:19 +0900

    메인 페이지 완료

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -21,8 +21,170 @@ export default function Main() {
                     <Select name="year"/>
                     <Select name="month"/>
                 </div>
-                <div>
-
+                <div className='infoArea'>
+                    <div className='graphArea'>
+                        <div></div>
+                        <ul>
+                            <li>사업지원팀</li>
+                            <li>본부관리팀</li>
+                            <li>외부영업</li>
+                            <li>강남주식팀</li>
+                            <li>대구코인팀</li>
+                            <li>대구주식팀</li>
+                            <li>문래지점</li>
+                            <li>청개구리주식스쿨</li>
+                            <li>청투TV</li>
+                        </ul>
+                    </div>
+                    <div className='boardBox'>
+                        <div className="board-top">
+                            <b>순위</b>
+                            <b>부서명</b>
+                            <b>전월 총 매출금액</b>
+                            <b>금월 총 매출금액</b>
+                            <b>전월대비</b>
+                        </div>
+                        <ol className="board-center">
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                        </ol>
+                        <div className="board-bottom">
+                            <b></b>
+                            <b>합계</b>
+                            <b>874,198,132</b>
+                            <b>874,198,132</b>
+                            <b>109.02%</b>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <ul>
+                    <li><button className='active'>상품별 매출</button></li>
+                    <li><button>애널리스트별 매출</button></li>
+                </ul>
+                <div className='selectArea'>
+                    <Select name="year"/>
+                    <Select name="month"/>
+                </div>
+                <div className='infoArea'>
+                    <div className='graphArea'>
+                        <div></div>
+                    </div>
+                    <div className='boardBox'>
+                        <div className="board-top">
+                            <b>순위</b>
+                            <b>부서명</b>
+                            <b>전월 총 매출금액</b>
+                            <b>금월 총 매출금액</b>
+                            <b>전월대비</b>
+                        </div>
+                        <ol className="board-center">
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                            <li>
+                                <span>1</span>
+                                <span>청개구리주식스쿨</span>
+                                <span>181,786,500</span>
+                                <span>181,786,500</span>
+                                <span>109.02%</span>
+                            </li>
+                        </ol>
+                        <div className="board-bottom">
+                            <b></b>
+                            <b>합계</b>
+                            <b>874,198,132</b>
+                            <b>874,198,132</b>
+                            <b>109.02%</b>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
