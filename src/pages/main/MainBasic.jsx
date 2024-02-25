c452b6fc140fd4624351b1452352db7b739b71f6
김성은 <kse012050@naver.com>
2024-02-25 10:18:27 +0900

    대시보드 데이터 없을 때 이미지 처리

--- src/pages/main/MainBasic.jsx
+++ src/pages/main/MainBasic.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { api } from '../../api/api';
 import SelectMonth from '../../components/SelectMonth';
 import SelectYear from '../../components/SelectYear';
+import { numberWithCommas } from '../../api/validation';
 
 export default function MainBasic() {
   
@@ -58,58 +59,64 @@ function DashboardFirst(){
                     <SelectMonth year={inputs.year} setInputs={setInputs} changeName='month' tab={tab}/>
                 }
             </div>
-            { !!dashboard?.length ? 
-                <div className='infoArea'>
-                    <div className='graphArea'>
-                        <div></div>
-                        <ul>
-                            <li>사업지원팀</li>
-                            <li>본부관리팀</li>
-                            <li>외부영업</li>
-                            <li>강남주식팀</li>
-                            <li>대구코인팀</li>
-                            <li>대구주식팀</li>
-                            <li>문래지점</li>
-                            <li>청개구리주식스쿨</li>
-                            <li>청투TV</li>
-                        </ul>
-                    </div>
-                    <div className='boardBox'>
-                            <>
-                                <div className="board-top">
-                                    <b>순위</b>
-                                    <b>부서명</b>
-                                    <b>전월 총 매출금액</b>
-                                    <b>금월 총 매출금액</b>
-                                    <b>전월대비</b>
-                                </div>
-                                <ol className="board-center">
-                                    {dashboard.map((data)=>(
-                                        <li key={data.ranking}>
-                                            <span>{ data.ranking }</span>
-                                            <span>{ data.department_name }</span>
-                                            <span>{ data.pre_month_sales_price }</span>
-                                            <span>{ data.current_month_sales_price }</span>
-                                            <span>{ data.percent && `${data.percent}%`}</span>
-                                        </li>
-                                    ))}
-                                </ol>
-                            </>
-                        { dashboardSum &&
-                            <div className="board-bottom">
-                                <b></b>
-                                <b>{ dashboardSum.department_name }</b>
-                                <b>{ dashboardSum.pre_month_sales_price }</b>
-                                <b>{ dashboardSum.current_month_sales_price }</b>
-                                <b>{ dashboardSum.percent && `${dashboardSum.percent}%` }</b>
-                            </div>
-                        }
-                    </div>
-                </div> :
-                <div>
-                    표시할 데이터 없음
+            <div className='infoArea'>
+                <div className='graphArea'>
+                    { !!dashboard?.length ? 
+                        <>
+                            <div className='test'></div>
+                            <ul>
+                                <li>사업지원팀</li>
+                                <li>본부관리팀</li>
+                                <li>외부영업</li>
+                                <li>강남주식팀</li>
+                                <li>대구코인팀</li>
+                                <li>대구주식팀</li>
+                                <li>문래지점</li>
+                                <li>청개구리주식스쿨</li>
+                                <li>청투TV</li>
+                            </ul> 
+                        </> :
+                        <div className='data-none'>
+                            표시할 데이터가 없습니다.
+                        </div>
+                    }
                 </div>
-            }
+                <div className='boardBox'>
+                    <div className="board-top">
+                        <b>순위</b>
+                        <b>부서명</b>
+                        <b>전월 총 매출금액</b>
+                        <b>금월 총 매출금액</b>
+                        <b>전월대비</b>
+                    </div>
+                    { !!dashboard?.length ?
+                        <ol className="board-center">
+                            {dashboard.map((data)=>(
+                                <li key={data.ranking}>
+                                    <span>{ data.ranking }</span>
+                                    <span>{ data.department_name }</span>
+                                    <span>{ data.pre_month_sales_price ? numberWithCommas(data.pre_month_sales_price) : 0 }</span>
+                                    <span>{ data.current_month_sales_price ? numberWithCommas(data.current_month_sales_price) : 0 }</span>
+                                    <span>{ data.percent && `${data.percent}%`}</span>
+                                </li>
+                            ))}
+                        </ol> :
+                        <div className='data-none'>
+                            표시할 데이터가 없습니다.
+                        </div>
+                    }
+                    <div className="board-bottom">
+                        <b></b>
+                        <b>합계</b>
+                        <b>{ dashboardSum?.pre_month_sales_price ? numberWithCommas(dashboardSum.pre_month_sales_price) : '-' }</b>
+                        <b>{ dashboardSum?.current_month_sales_price ? numberWithCommas(dashboardSum.current_month_sales_price) : '-' }</b>
+                        <b>{ dashboardSum?.percent ? `${dashboardSum.percent}%` : '-' }</b>
+                    </div>
+                </div>
+            </div>
+            {/* <div>
+                표시할 데이터 없음
+            </div> */}
         </div>
     )
 }
@@ -135,9 +142,9 @@ function DashboardSecond(){
                 api('dashboard', funcType, {'stat_date': `${inputs.year}-${inputs.month}`})
                     .then(({result, list})=>{
                         if(result){
-                            // console.log(list);
-                            setDashboard(list.filter(({ranking})=>ranking))
-                            setDashboardSum(list.filter(({ranking})=>!ranking)[0])
+                            console.log(list);
+                            setDashboard(list ? list.filter(({ranking})=>ranking) : [])
+                            setDashboardSum(list ? list.filter(({ranking})=>!ranking)[0] : [])
                             // console.log(list.filter(({ranking})=>!ranking)[0]);
                         }
                     })
@@ -160,51 +167,58 @@ function DashboardSecond(){
             </div>
             <div className='infoArea'>
                 <div className='graphArea'>
-                    <div></div>
-                    <ul>
-                        <li>사업지원팀</li>
-                        <li>본부관리팀</li>
-                        <li>외부영업</li>
-                        <li>강남주식팀</li>
-                        <li>대구코인팀</li>
-                        <li>대구주식팀</li>
-                        <li>문래지점</li>
-                        <li>청개구리주식스쿨</li>
-                        <li>청투TV</li>
-                    </ul>
-                </div>
-                <div className='boardBox'>
-                    { !!dashboard?.length && 
+                    { !!dashboard?.length ? 
                         <>
-                            <div className="board-top">
-                                <b>순위</b>
-                                <b>상품명</b>
-                                <b>매출건수</b>
-                                <b>금월 총 매출금액</b>
-                                <b>매출점유율</b>
-                            </div>
-                            <ol className="board-center">
-                                {dashboard.map((data)=>(
-                                    <li key={data.ranking}>
-                                        <span>{ data.ranking }</span>
-                                        <span>{ data.product_name || data.analyst_admin_name }</span>
-                                        <span>{ data.total_count }</span>
-                                        <span>{ data.total_price }</span>
-                                        <span>{ data.percent && `${data.percent}%`}</span>
-                                    </li>
-                                ))}
-                            </ol>
-                        </>
-                    }
-                    { dashboardSum &&
-                        <div className="board-bottom">
-                            <b></b>
-                            <b>{ dashboardSum.product_name }</b>
-                            <b>{ dashboardSum.total_count }</b>
-                            <b>{ dashboardSum.total_price }</b>
-                            <b>{ dashboardSum.percent && `${dashboardSum.percent}%` }{dashboardSum.percent}</b>
+                            <div className='test'></div>
+                            <ul>
+                                <li>사업지원팀</li>
+                                <li>본부관리팀</li>
+                                <li>외부영업</li>
+                                <li>강남주식팀</li>
+                                <li>대구코인팀</li>
+                                <li>대구주식팀</li>
+                                <li>문래지점</li>
+                                <li>청개구리주식스쿨</li>
+                                <li>청투TV</li>
+                            </ul> 
+                        </> :
+                        <div className='data-none'>
+                            표시할 데이터가 없습니다.
                         </div>
                     }
+                </div>
+                <div className='boardBox'>
+                    <div className="board-top">
+                        <b>순위</b>
+                        <b>상품명</b>
+                        <b>매출건수</b>
+                        <b>금월 총 매출금액</b>
+                        <b>매출점유율</b>
+                    </div>
+                    { !!dashboard?.length ?
+                        <ol className="board-center">
+                            {dashboard.map((data)=>(
+                                <li key={data.ranking}>
+                                    <span>{ data.ranking }</span>
+                                    <span>{ data.product_name || data.analyst_admin_name }</span>
+                                    <span>{ data.total_count }</span>
+                                    <span>{ data.total_price ? numberWithCommas(data.total_price) : 0 }</span>
+                                    <span>{ data.percent && `${data.percent}%`}</span>
+                                </li>
+                            ))}
+                        </ol> :
+                        <div className='data-none'>
+                            표시할 데이터가 없습니다.
+                        </div>
+                    }
+                    
+                    <div className="board-bottom">
+                        <b></b>
+                        <b>합계</b>
+                        <b>{ dashboardSum?.pre_month_sales_price ? numberWithCommas(dashboardSum.pre_month_sales_price) : '-' }</b>
+                        <b>{ dashboardSum?.current_month_sales_price ? numberWithCommas(dashboardSum.current_month_sales_price) : '-' }</b>
+                        <b>{ dashboardSum?.percent ? `${dashboardSum.percent}%` : '-' }</b>
+                    </div>
                 </div>
             </div>
         </div>
