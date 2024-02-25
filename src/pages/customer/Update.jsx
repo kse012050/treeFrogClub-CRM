c369e26b4d9c9cc276d929e12767a8a612b8b605
김성은 <kse012050@naver.com>
2024-02-26 04:25:09 +0900

    통합 DB 관리 - 보기 페이지 게시판 추가 수정

--- src/pages/customer/Update.jsx
+++ src/pages/customer/Update.jsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useContext, useEffect, useId, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { api } from '../../api/api';
-import { inputChange } from '../../api/validation';
+import { inputChange, numberWithCommas } from '../../api/validation';
 import { DatePicker } from 'antd';
 import DropBox from '../../components/DropBox';
 import Select from '../../components/Select';
@@ -429,9 +429,12 @@ function History({ id, historyPaymentFunc, paymentInfo, counselValue, historyPay
     const [inputs, setInputs] = useState({'limit': '10', 'page': '1', 'customer_id': id});
     const [historyUpdata, setHistoryUpdata] = useState()
     const [updateInfo, setUpdateInfo] = useState()
+    const [historyPaymentDelete, setHistoryPaymentDelete] = useState()
+    const [historyPaymentDeleteInfo, setHistoryPaymentDeleteInfo] = useState()
     const [refundPopupActive, setRefundPopupActive] = useState()
     const [updatePopupActive, setUpdatePopupActive] = useState()
     const [consultCount, setConsultCount] = useState()
+    const [deleteList, setDeleteList] = useState([])
 
     const historyUpdateFunc = useCallback(()=>{
         api('payment','user_payment_history_list', inputs)
@@ -444,10 +447,22 @@ function History({ id, historyPaymentFunc, paymentInfo, counselValue, historyPay
             })
     },[inputs])
 
+    const historyPaymentDeleteFunc = useCallback(()=>{
+        api('payment','user_delete_payment_list', inputs)
+            .then(({result, data, list})=>{
+                if(result){
+                    setHistoryPaymentDeleteInfo(data)
+                    setHistoryPaymentDelete(list)
+                    // console.log(list);
+                }
+            })
+    },[inputs])
+
     useEffect(()=>{
         historyPaymentFunc(inputs)
         historyUpdateFunc(inputs)
-    },[inputs, historyPaymentFunc, historyUpdateFunc])
+        historyPaymentDeleteFunc(inputs)
+    },[inputs, historyPaymentFunc, historyUpdateFunc, historyPaymentDeleteFunc])
 
     return(
         <>
@@ -456,17 +471,20 @@ function History({ id, historyPaymentFunc, paymentInfo, counselValue, historyPay
                     <button data-count={consultCount} className={relatedActive === 0 ? 'active' : ''} onClick={()=>setRelatedActive(0)}>상담이력</button>
                     <button data-count={paymentInfo?.total_count} className={relatedActive === 1 ? 'active' : ''} onClick={()=>setRelatedActive(1)}>결제내역</button>
                     <button data-count={updateInfo?.total_count} className={relatedActive === 2 ? 'active' : ''} onClick={()=>setRelatedActive(2)}>결제수정내역</button>
-                    <button data-count="0" className={relatedActive === 3 ? 'active' : ''} onClick={()=>setRelatedActive(3)}>삭제된 결제내역</button>
+                    <button data-count={historyPaymentDeleteInfo?.total_count} className={relatedActive === 3 ? 'active' : ''} onClick={()=>setRelatedActive(3)}>삭제된 결제내역</button>
 
                     {relatedActive === 0 &&
                         <HistoryConsult id={id} setConsultCount={setConsultCount} counselValue={counselValue}/>
                     }
                     {relatedActive === 1 &&
                         <>
+                            <b className='choice'>{ deleteList.length }</b>
+                            <BoardChkDelete url='payment' idName='payment_id_list' deleteList={deleteList} setDeleteList={setDeleteList} currentData={()=>{historyPaymentFunc(inputs); historyPaymentDeleteFunc(inputs);}}/>
                             <b className='total'>{ paymentInfo?.total_count }</b>
                             <span className='page'>{ paymentInfo?.current_page }/{ paymentInfo?.total_page }</span>
                             <div className='board-scroll2'>
                                 <div className="board-top">
+                                    <BoardChkAll deleteList={deleteList} setDeleteList={setDeleteList} list={historyPayment?.map(({payment_id})=>payment_id)} />
                                     <button>결제번호</button>
                                     <button>결제구분</button>
                                     <button>
@@ -491,14 +509,15 @@ function History({ id, historyPaymentFunc, paymentInfo, counselValue, historyPay
                                     <ol className="board-center">
                                         { historyPayment.map((data)=>(
                                             <li key={ data.payment_id }>
+                                                <BoardChk id={data.payment_id} deleteList={deleteList} setDeleteList={setDeleteList}/>
                                                 <span>{ data.payment_id }</span>
                                                 <span>{ data.payment_properties_name }</span>
                                                 <span>{ data.payment_person_in_charge_name }</span>
                                                 <span>{ data.product_name.replaceAll(' ','\n') }</span>
                                                 <time>{ data.payment_date.replaceAll('-','/') }</time>
-                                                <span>{ data.payment_price }</span>
-                                                <time>{ data?.refund_date?.replaceAll('-','/') }</time>
-                                                <span>{ data?.refund_price }</span>
+                                                <span>{ data.payment_price ? numberWithCommas(data.payment_price) : '' }</span>
+                                                <time>{ data?.refund_date ? data?.refund_date?.replaceAll('-','/') : '' }</time>
+                                                <span>{ data?.refund_price ? numberWithCommas(data?.refund_price) : ''}</span>
                                                 <time>
                                                     { data.standard_service_start_date.replaceAll('-','/') }<br/>
                                                     ~ { data.standard_service_end_date.replaceAll('-','/') }
@@ -538,9 +557,9 @@ function History({ id, historyPaymentFunc, paymentInfo, counselValue, historyPay
                                             <li key={ i }>
                                                 <span>{ data.payment_id }</span>
                                                 <span>{ data.item }</span>
-                                                <time>{ data.modify_before_info.replaceAll('-', '/') }</time>
-                                                <time>{ data.modify_after_info.replaceAll('-', '/') }</time>
-                                                <time>{ data.reg_date.split(' ')[0] }</time>
+                                                <time>{ data.modify_before_info ? numberWithCommas(data.modify_before_info) : '' }</time>
+                                                <time>{ data.modify_after_info ? numberWithCommas(data.modify_after_info) : '' }</time>
+                                                <time>{ data.reg_date.split(' ')[0].replaceAll('-','/') }</time>
                                                 <span>{ data.modify_admin_name }</span>
                                             </li>
                                         ))}
@@ -553,7 +572,60 @@ function History({ id, historyPaymentFunc, paymentInfo, counselValue, historyPay
                         </>
                     }
                     {relatedActive === 3 &&
-                        <HistoryDelete />
+                        <>
+                            <div className='board-scroll4'>
+                                <div className="board-top">
+                                    <button>결제번호</button>
+                                    <button>결제구분</button>
+                                    <button>
+                                        신청<br/>
+                                        애널리스트
+                                    </button>
+                                    <button>결제일</button>
+                                    <button>결제금액</button>
+                                    <button>환불일</button>
+                                    <button>환불금액</button>
+                                    <button>
+                                        유료기간<br/>
+                                        (결제기준)
+                                    </button>
+                                    <button>
+                                        유료기간<br/>
+                                        (서비스기간포함)
+                                    </button>
+                                    <button>삭제일</button>
+                                    <button>삭제자</button>
+                                </div>
+                                { historyPaymentDelete && 
+                                    <ol className="board-center">
+                                        { historyPaymentDelete.map((data)=>
+                                            <li key={ data.payment_id }>
+                                                <span>{ data.payment_id }</span>
+                                                <span>{ data.payment_properties_name }</span>
+                                                <span>{ data.product_name }</span>
+                                                <time>{ data?.payment_date.replaceAll('-','/') }</time>
+                                                <span>{ data.payment_price ? numberWithCommas(data.payment_price) : '0' }</span>
+                                                <span>{ data?.refund_date?.replaceAll('-','/') }</span>
+                                                <span>{ data.refund_price ? numberWithCommas(data.refund_price) : '0'}</span>
+                                                <time>
+                                                    { data.standard_payment_start_date.replaceAll('-','/') }<br/>
+                                                    ~{ data.standard_payment_end_date.replaceAll('-','/') }
+                                                </time>
+                                                <time>
+                                                    { data.standard_service_start_date.replaceAll('-','/') }<br/>
+                                                    ~{ data.standard_service_end_date.replaceAll('-','/') }
+                                                </time>
+                                                <time>{ data?.del_date.split(' ')[0].replaceAll('-','/') }</time>
+                                                <span>{ data.del_admin_name }</span>
+                                            </li>
+                                        )}
+                                    </ol>
+                                }
+                            </div>
+                            <div className='board-pagination' data-styleidx='a'>
+                                <Pager pagerInfo={historyPaymentDeleteInfo} setInputs={setInputs}/>
+                            </div>
+                        </>
                     }
 
                     
@@ -809,72 +881,72 @@ function HistoryConsultInput({ id, currentData, counselValue }){
     )
 }
 
-function HistoryDelete(){
-    return (
-        <>
-            <div className='board-scroll4'>
-                <div className="board-top">
-                    <button>결제번호</button>
-                    <button>결제구분</button>
-                    <button>
-                        신청<br/>
-                        애널리스트
-                    </button>
-                    <button>결제일</button>
-                    <button>결제금액</button>
-                    <button>환불일</button>
-                    <button>환불금액</button>
-                    <button>
-                        유료기간<br/>
-                        (결제기준)
-                    </button>
-                    <button>
-                        유료기간<br/>
-                        (서비스기간포함)
-                    </button>
-                    <button>삭제일</button>
-                    <button>삭제자</button>
-                </div>
-                <ol className="board-center">
-                    <li>
-                        <span>123456</span>
-                        <span>
-                            카드/현금<br/>
-                            분할결제
-                        </span>
-                        <span>
-                            [청투TV]<br/>
-                            재료주헌터
-                        </span>
-                        <time>2023/09/27</time>
-                        <span>300,000</span>
-                        <span></span>
-                        <span></span>
-                        <time>
-                            2023/10/01<br/>
-                            ~2023/11/01
-                        </time>
-                        <time>
-                            2023/10/01<br/>
-                            ~2023/11/01
-                        </time>
-                        <time>2023/09/27</time>
-                        <span>홍길동</span>
-                    </li>
-                </ol>
-            </div>
-            <div className='board-pagination' data-styleidx='a'>
-                <Link to={''}>첫 페이지</Link>
-                <Link to={''}>이전 페이지</Link>
-                <ol>
-                    <li className='active'><Link to={''}>1</Link></li>
-                </ol>
-                <Link to={''}>다음 페이지</Link>
-                <Link to={''}>마지막 페이지</Link>
-            </div>
-        </>
-    )
-}
+// function HistoryDelete(){
+//     return (
+//         <>
+//             <div className='board-scroll4'>
+//                 <div className="board-top">
+//                     <button>결제번호</button>
+//                     <button>결제구분</button>
+//                     <button>
+//                         신청<br/>
+//                         애널리스트
+//                     </button>
+//                     <button>결제일</button>
+//                     <button>결제금액</button>
+//                     <button>환불일</button>
+//                     <button>환불금액</button>
+//                     <button>
+//                         유료기간<br/>
+//                         (결제기준)
+//                     </button>
+//                     <button>
+//                         유료기간<br/>
+//                         (서비스기간포함)
+//                     </button>
+//                     <button>삭제일</button>
+//                     <button>삭제자</button>
+//                 </div>
+//                 <ol className="board-center">
+//                     <li>
+//                         <span>123456</span>
+//                         <span>
+//                             카드/현금<br/>
+//                             분할결제
+//                         </span>
+//                         <span>
+//                             [청투TV]<br/>
+//                             재료주헌터
+//                         </span>
+//                         <time>2023/09/27</time>
+//                         <span>300,000</span>
+//                         <span></span>
+//                         <span></span>
+//                         <time>
+//                             2023/10/01<br/>
+//                             ~2023/11/01
+//                         </time>
+//                         <time>
+//                             2023/10/01<br/>
+//                             ~2023/11/01
+//                         </time>
+//                         <time>2023/09/27</time>
+//                         <span>홍길동</span>
+//                     </li>
+//                 </ol>
+//             </div>
+//             <div className='board-pagination' data-styleidx='a'>
+//                 <Link to={''}>첫 페이지</Link>
+//                 <Link to={''}>이전 페이지</Link>
+//                 <ol>
+//                     <li className='active'><Link to={''}>1</Link></li>
+//                 </ol>
+//                 <Link to={''}>다음 페이지</Link>
+//                 <Link to={''}>마지막 페이지</Link>
+//             </div>
+//         </>
+//     )
+// }
 
 
 function RefundPopup({ refundPopupActive, setRefundPopupActive }){
